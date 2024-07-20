df45bcb81db4ce58699862d255071b708d4bb77e
ChillyTheEevee <myersdan@studentmail.conroeisd.net>
2024-07-19 21:03:38 -0500

    Created the MolehuntPlus project through IntelliJ IDEA

---
+++ src/main/java/live/chillytheeevee/molehuntplus/MolehuntPlus.java
@@ -0,0 +1,17 @@
+package live.chillytheeevee.molehuntplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MolehuntPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
